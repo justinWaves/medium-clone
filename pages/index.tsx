97149ae6a2ee5b19fe7ceef02b7e5e20a0bd34ff
justinWaves <juhstinn@gmail.com>
2022-03-30 16:38:54 -0700

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../Components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="mx-auto max-w-7xl">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{' '}
+            is a place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img
+          className="hidden h-32 md:inline-flex lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Home
