2e2381bc6efac9309b8de4ab059064a7d5884884
justinWaves <juhstinn@gmail.com>
2022-03-31 01:40:08 -0700

    Finished initial build, ready to deploy

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,13 +3,44 @@ import Header from '../../Components/Header'
 import { GetStaticProps } from 'next'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
 
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
+  const [submitted, setSubmitted] = useState(false)
+
   console.log(post)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    await fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setSubmitted(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSubmitted(false)
+      })
+  }
 
   return (
     <main>
@@ -65,23 +96,90 @@ function Post({ post }: Props) {
 
       <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
 
-      <form className="mx-auto mb-10 flex max-w-2xl flex-col p-5 ">
-        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
-        <hr className="mt-2 py-3" />
-        <label>
-          <span>Name</span>
-          <input placeholder="John Appleseed" type="text" />
-        </label>
-        <label>
-          <span>Email</span>
-          <input placeholder="jappleseed@gmail.com" type="text" />
-        </label>
-        <label>
-          <span>Comment</span>
-          <textarea placeholder="John Appleseed" rows={8} />
-        </label>
-      </form>
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+          <h3 className="text-3xl font-bold">
+            Thank you for submitted your comment!
+          </h3>
+          <p>Once it has been approved, it will appear below!</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5 "
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="mt-2 py-3" />
+
+          <input
+            {...register('_id')}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="John Appleseed"
+              type="text"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register('email', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="jappleseed@gmail.com"
+              type="email"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="John Appleseed"
+              rows={8}
+            />
+          </label>
+          {/* Errors will return when field validation fails below */}
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">The Name field is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">The Email field is required</span>
+            )}
+
+            {errors.comment && (
+              <span className="text-red-500">
+                The Comment field is required
+              </span>
+            )}
+          </div>
+          <input
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 text-white shadow hover:bg-yellow-400 focus:outline-none"
+            type="submit"
+          />
+        </form>
+      )}
+      {/* COMMENTS */}
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>{' '}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
@@ -119,6 +217,10 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         name,
         image
       }, 
+      'comments': *[
+        _type =="comment" && 
+        post._ref == ^._id &&
+        approved == true],
       description,
       mainImage,
       slug,
