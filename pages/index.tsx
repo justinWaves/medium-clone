e0847382ef2af115959ef7d3e0f8db3f8866be00
justinWaves <juhstinn@gmail.com>
2022-03-31 02:03:44 -0700

    debugging Header issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import Header from '../Components/Header'
+import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
 
