5de223632ab2419622605a9cc36f1d51af8b4691
justinWaves <juhstinn@gmail.com>
2022-03-31 17:11:59 -0700

    Debugging vercel deploy pt 4

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -5,6 +5,7 @@ import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { useState } from 'react'
+import Link from 'next/link'
 
 interface IFormInput {
   _id: string
@@ -44,7 +45,34 @@ function Post({ post }: Props) {
 
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
+
+      <header className="mx-auto flex max-w-7xl justify-between p-5">
+        <div className="flex items-center space-x-5">
+          <Link href="/">
+            <img
+              className="w-44 cursor-pointer object-contain"
+              src="https://links.papareact.com/yvf"
+              alt=""
+            />
+          </Link>
+          <div className="hidden items-center space-x-5 md:inline-flex">
+            <h3>About</h3>
+            <h3>Contact</h3>
+            <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+              Follow
+            </h3>
+          </div>
+        </div>
+        <div className="flex items-center space-x-5 text-green-600">
+          <h3>Sign In</h3>
+          <h3 className="rounded-full border border-green-600 px-4 py-1">
+            Get Started
+          </h3>
+        </div>
+      </header>
+
+      {/* ^^^^^^ debugging header deploy issue ^^^^ */}
 
       <img
         className="h-40 w-full object-cover"
